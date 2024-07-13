18f3d350013015be06825a539197045d9a9e82c0
sebkolind <sks1993@gmail.com>
2024-07-13 22:56:22 +0200

    example

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,12 +3,5 @@ import { draggy } from "@sebkolind/draggy";
 draggy({
   draggable: ".draggable",
   dropzone: ".dropzone",
-  isDropzone: ({ el }) => el.classList.contains("dropzone"),
-  onDrop: () => {
-    console.log("I was dropped!");
-
-    return ({ dragged, target }) => {
-      target.append(dragged);
-    };
-  },
+  isDropzone: ({ el }) => el.classList.contains("draggy-dropzone"),
 });
