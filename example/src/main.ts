775123c9839656444090464cdfa14f09a0150b48
sebkolind <sks1993@gmail.com>
2024-07-20 23:46:45 +0200

    update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,27 +2,5 @@ import { draggy } from "@sebkolind/draggy";
 
 draggy({
   target: ".column",
-  optimistic: false,
-  onDrop(_, { dragged, dropzone }) {
-    const status = dropzone?.dataset.status;
-    const el = dragged.querySelector(".status");
-    if (el && status) {
-      el.classList.remove("to-do", "in-progress", "done");
-      el.classList.add(status);
-      el.textContent = formatStatus(status);
-    }
-  },
+  placement: "any",
 });
-
-const formatStatus = (status: string) => {
-  switch (status) {
-    case "to-do":
-      return "To-do";
-    case "in-progress":
-      return "In Progress";
-    case "done":
-      return "Done";
-    default:
-      return status;
-  }
-};
