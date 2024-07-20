74f31c07b145d4caac6132066507db8ae62a57d1
sebkolind <sks1993@gmail.com>
2024-07-20 20:54:51 +0200

    update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,4 +2,26 @@ import { draggy } from "@sebkolind/draggy";
 
 draggy({
   target: ".column",
+  onDrop(_, { dragged, dropzone }) {
+    const status = dropzone?.dataset.status;
+    const el = dragged.querySelector(".status");
+    if (el && status) {
+      el.classList.remove("to-do", "in-progress", "done");
+      el.classList.add(status);
+      el.textContent = formatStatus(status);
+    }
+  },
 });
+
+const formatStatus = (status: string) => {
+  switch (status) {
+    case "to-do":
+      return "To-do";
+    case "in-progress":
+      return "In Progress";
+    case "done":
+      return "Done";
+    default:
+      return status;
+  }
+};
