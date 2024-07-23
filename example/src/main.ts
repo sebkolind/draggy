aeaac195245007672a44e70b7807e30fd5670baf
sebkolind <sks1993@gmail.com>
2024-07-23 15:44:26 +0200

    update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,4 +3,22 @@ import { draggy } from "@sebkolind/draggy";
 draggy({
   target: ".column",
   placement: "any",
+  onShadow(_, { shadow, multiple }) {
+    if (!multiple?.length) return;
+
+    const label = document.createElement("div");
+    label.textContent = multiple.length.toString();
+    label.style.position = "absolute";
+    label.style.top = "0";
+    label.style.right = "0";
+    label.style.transform = "translate(50%, -50%)";
+    label.style.backgroundColor = "red";
+    label.style.color = "white";
+    label.style.borderRadius = "50%";
+    label.style.padding = "2px 6px";
+    label.style.fontSize = "12px";
+    label.style.zIndex = "9999";
+
+    shadow?.append(label);
+  },
 });
